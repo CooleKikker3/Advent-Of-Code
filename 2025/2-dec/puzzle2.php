205cc501dd6c991bfec15577673c889a07fdc818
CooleKikker3 <coenvink2003@gmail.com>
2025-12-02 10:37:57 +0100

    2025, day 2, part 2

--- 2025/2-dec/puzzle2.php
+++ 2025/2-dec/puzzle2.php
@@ -1,39 +1,68 @@
 <?php
-    error_reporting(E_ALL);
+error_reporting(E_ALL);
 
-    $input = file_get_contents("input1.txt");
-    $lines = explode(",", $input);
+$input = file_get_contents("input1.txt");
+$lines = explode(",", $input);
 
-    $answer = 0;
-    foreach ($lines as $range)
+$answer = 0;
+foreach ($lines as $range)
+{
+    $defined_range = explode("-", $range);
+    $min = $defined_range[0];
+    $max = $defined_range[1];
+
+    $current = $min;
+    while($current <= $max)
     {
-        $defined_range = explode("-", $range);
-        $min = $defined_range[0];
-        $max = $defined_range[1];
+        $split = strlen($current);
 
-        $current = $min;
-        while($current <= $max)
+        if($split % 2 == 0)
         {
-            $split = strlen($current);
-
-            if($split % 2 != 0)
-            {
-                $current++;
-                continue;
-            }
-
             $split_number = str_split($current, $split / 2);
-
             if($split_number[0] == $split_number[1])
             {
                 $answer += $current;
 
                 echo $current . "<br>";
+                $current++;
+                continue;
             }
+        }
+
+
+        // Part 2 extension
+        $split_number1 = str_split($current);
+        $split_number2 = str_split($current, 2);
+        $split_number3 = str_split($current, 3);
+
+
+        if(AllSameNumbers($split_number1) || AllSameNumbers($split_number2) || AllSameNumbers($split_number3))
+        {
+            echo $current . "<br>";
+            $answer += $current;
 
             $current++;
         }
+
+
+
+
+        $current++;
     }
-    echo "<b>" . $answer . "</b>";
+}
+echo "<b>" . $answer . "</b>";
 
+function AllSameNumbers(array $arr) {
+    if(count($arr) < 2)
+    {
+        return false;
+    }
 
+    $testValue = $arr[0];
+    foreach ($arr as $val) {
+        if ($testValue !== $val) {
+            return false;
+        }
+    }
+    return true;
+}
